2fdd3484d2754053e911ca9288a9bab16eed719e
mswift42 <martin.haesler@gmail.com>
2017-01-28 12:35:04 +0000

    Add Bind method

--- app/src/main/java/mswift42/com/github/eggcrackin/RecipesViewAdapter.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/RecipesViewAdapter.java
@@ -1,7 +1,13 @@
 package mswift42.com.github.eggcrackin;
 
 
+import android.content.Context;
+import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RecipesViewAdapter extends RecyclerView.Adapter<> {
 
@@ -21,4 +27,33 @@ public class RecipesViewAdapter extends RecyclerView.Adapter<> {
         mOnClickListener = listener;
         viewHolderCount = 0;
     }
+    class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView mRecipeTitle;
+
+        @Override
+        public RecipeViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            Context context = viewGroup.getContext();
+            int layoutIdForListItem = R.layout.recipe_card;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
+
+            View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+
+            RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+
+        }
+        void bind(int listIndex) {
+            String title = MockRecipeData.getRecipes().get(listIndex).getTitle();
+            mRecipeTitle.setText(title);
+        }
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+
+            mRecipeTitle = (TextView) itemView.findViewById(R.id.ec_recipe_title);
+            // COMPLETED (7) Call setOnClickListener on the View passed into the constructor (use 'this' as the OnClickListener)
+            itemView.setOnClickListener(this);
+        }
+    }
+
 }
