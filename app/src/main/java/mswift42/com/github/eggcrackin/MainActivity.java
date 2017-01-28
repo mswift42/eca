9c74887ac41c241a1c17bfca1490f8c7401a4133
mswift42 <martin.haesler@gmail.com>
2017-01-28 13:04:14 +0000

    Set layoutmanager for recipe list.

--- app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.java
@@ -2,12 +2,21 @@ package mswift42.com.github.eggcrackin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements RecipesAdapter.ListItemClickListener {
 
+    private RecipesAdapter mAdapter;
+    private RecyclerView mRecipeList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecipeList = (RecyclerView) findViewById(R.id.ec_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecipeList.setLayoutManager(layoutManager);
     }
 }
