bdca05bd475daa38106a0a11a85c81b65e815f9b
mswift42 <martin.haesler@gmail.com>
2017-02-01 13:49:45 +0000

    Add getter / setter for Recipe[].

--- app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
@@ -2,4 +2,13 @@ package mswift42.com.github.eggcrackin;
 
 
 public class Favourites {
+    private Recipe[] recipes;
+
+    public Recipe[] getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Recipe[] recipes) {
+        this.recipes = recipes;
+    }
 }
