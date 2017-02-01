584e2e40c48395fbb7c617c15ca942366e3cca50
mswift42 <martin.haesler@gmail.com>
2017-02-01 13:53:30 +0000

    add methods for delete / add

--- app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
@@ -1,14 +1,24 @@
 package mswift42.com.github.eggcrackin;
 
 
-public class Favourites {
-    private Recipe[] recipes;
+import java.util.List;
 
-    public Recipe[] getRecipes() {
+public class Favourites {
+    private List<Recipe> recipes;
+
+    public List<Recipe> getRecipes() {
         return recipes;
     }
 
-    public void setRecipes(Recipe[] recipes) {
+    public void setRecipes(List<Recipe> recipes) {
         this.recipes = recipes;
     }
+    public void addFavourite(Recipe recipe) {
+        recipes.add(recipe);
+    }
+
+    public void deleteFavourite(Recipe recipe) {
+        recipes.remove(recipe);
+    }
+
 }
