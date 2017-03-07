30da088ddfa97b037a3bb908a99873f5b3b19110
mswift42 <martin.haesler@gmail.com>
2017-03-07 14:26:26 +0000

    Check initial favourites list is empty.

--- app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
+++ app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
@@ -1,5 +1,12 @@
 package mswift42.com.github.eggcrackin;
 
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class FavouritesUnitTest {
+    @Test
+    public void favourites_set_empty_recipe_list() throws Exception {
+        Favourites favs = new Favourites();
+        assertEquals(favs.getRecipes().size(), 0);
+    }
 }
