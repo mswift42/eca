522e6be5f89e6767e43da09be5dca535f2e5a21f
mswift42 <martin.haesler@gmail.com>
2017-03-07 16:21:43 +0000

    Check addFavourite method.

--- app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
+++ app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
@@ -2,11 +2,29 @@ package mswift42.com.github.eggcrackin;
 
 
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 public class FavouritesUnitTest {
     @Test
     public void favourites_set_empty_recipe_list() throws Exception {
         Favourites favs = new Favourites();
         assertEquals(favs.getRecipes().size(), 0);
+    }
+
+    @Test
+    public void setrecipes_sets_recipes_to_favourites_list() throws Exception {
+        Favourites favs = new Favourites();
+        favs.setRecipes(MockRecipeData.getRecipes());
+        assertEquals(favs.getRecipes().size(), 5);
+    }
+
+    @Test
+    public void addFavourites_adds_recipe_to_favourites() throws Exception {
+        Favourites favs = new Favourites();
+        List<Recipe> mr = MockRecipeData.getRecipes();
+        favs.addFavourite(mr.get(0));
+        assertEquals(favs.getRecipes().size(),1);
     }
 }
