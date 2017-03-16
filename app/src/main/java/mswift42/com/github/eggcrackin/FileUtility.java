168e3274cf494457e68e876075a3f3663b0dbb84
mswift42 <martin.haesler@gmail.com>
2017-03-16 12:45:58 +0000

    Add restoreFromFile method.

--- app/src/main/java/mswift42/com/github/eggcrackin/FileUtility.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/FileUtility.java
@@ -3,25 +3,51 @@ package mswift42.com.github.eggcrackin;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class FileUtility {
-   Context mContext;
-   FileOutputStream outputStream;
-   String savedFavourites = "favourites.json";
+    Context mContext;
+    FileOutputStream outputStream;
+    String savedFavourites = "favourites.json";
 
-   FileUtility(Context context) {
-      mContext = context;
-   }
+    FileUtility(Context context) {
+        mContext = context;
+    }
 
-   public void writeToFile(String content) {
-      try {
-         outputStream = mContext.getApplicationContext().openFileOutput(savedFavourites, Context.MODE_PRIVATE);
-         outputStream.write(content.getBytes());
-         outputStream.close();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+    public void writeToFile(String content) {
+        try {
+            outputStream = mContext.getApplicationContext()
+                    .openFileOutput(savedFavourites, Context.MODE_PRIVATE);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String restoreFromFile(String content) throws FileNotFoundException {
+        try {
+            FileInputStream fi = mContext.getApplicationContext()
+                    .openFileInput(savedFavourites);
+            InputStreamReader isr = new InputStreamReader(fi);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new FileNotFoundException();
+        }
+    }
 
 }
+
+
+
