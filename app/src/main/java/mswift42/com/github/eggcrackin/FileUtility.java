105d64dcb27325e8d471a368d83e56088826a23c
mswift42 <martin.haesler@gmail.com>
2017-03-16 13:46:18 +0000

    change field outputstream to local variable.

--- app/src/main/java/mswift42/com/github/eggcrackin/FileUtility.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/FileUtility.java
@@ -7,21 +7,16 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class FileUtility {
-    Context mContext;
-    FileOutputStream outputStream;
-    String savedFavourites = "favourites.json";
+    private static final String savedFavourites = "favourites.json";
 
-    FileUtility(Context context) {
-        mContext = context;
-    }
 
-    public void writeToFile(String content) {
+    public static void writeToFile(Context context, String content) {
+        FileOutputStream outputStream;
         try {
-            outputStream = mContext.getApplicationContext()
+            outputStream = context.getApplicationContext()
                     .openFileOutput(savedFavourites, Context.MODE_PRIVATE);
             outputStream.write(content.getBytes());
             outputStream.close();
@@ -30,9 +25,9 @@ public class FileUtility {
         }
     }
 
-    public String restoreFromFile(String content) throws FileNotFoundException {
+    public static String restoreFromFile(Context context, String content) throws FileNotFoundException {
         try {
-            FileInputStream fi = mContext.getApplicationContext()
+            FileInputStream fi = context.getApplicationContext()
                     .openFileInput(savedFavourites);
             InputStreamReader isr = new InputStreamReader(fi);
             BufferedReader br = new BufferedReader(isr);
