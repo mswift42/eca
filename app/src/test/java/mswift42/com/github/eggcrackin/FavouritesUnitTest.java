8ea0d9a7d438f232e465ffa6fefc36e2a7208714
mswift42 <martin.haesler@gmail.com>
2017-03-20 11:31:19 +0000

    Fix tests to use singleton pattern.

--- app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
+++ app/src/test/java/mswift42/com/github/eggcrackin/FavouritesUnitTest.java
@@ -9,42 +9,49 @@ import static org.junit.Assert.*;
 public class FavouritesUnitTest {
     @Test
     public void favourites_set_empty_recipe_list() throws Exception {
-        Favourites favs = new Favourites();
-        assertEquals(favs.getFavourites().size(), 0);
+        assertEquals(Favourites.getInstance().getmFavourites().size(), 0);
     }
 
     @Test
     public void setrecipes_sets_recipes_to_favourites_list() throws Exception {
-        Favourites favs = new Favourites();
-        favs.setFavourites(MockRecipeData.getRecipes());
-        assertEquals(favs.getFavourites().size(), 4);
+        Favourites.getInstance().setmFavourites(MockRecipeData.getRecipes());
+        assertEquals(Favourites.getInstance().getmFavourites().size(), 5);
+
     }
 
     @Test
     public void addFavourites_adds_recipe_to_favourites() throws Exception {
-        Favourites favs = new Favourites();
         List<Recipe> mr = MockRecipeData.getRecipes();
-        favs.addFavourite(mr.get(0));
-        assertEquals(favs.getFavourites().size(),1);
+        Favourites.getInstance().addFavourite(mr.get(0));
+        assertEquals(Favourites.getInstance().getmFavourites().size(),1);
     }
 
     @Test
     public void deleteFavourite_deletes_favourite() throws Exception {
-        Favourites favs = new Favourites();
         List<Recipe> mr = MockRecipeData.getRecipes();
-        favs.setFavourites(mr);
-        assertEquals(favs.getFavourites().size(),5);
-        boolean removed = favs.deleteFavourite(mr.get(0));
+        Favourites.getInstance().setmFavourites(mr);
+        assertEquals(Favourites.getInstance().getmFavourites().size(),5);
+        boolean removed = Favourites.getInstance().deleteFavourite(mr.get(0));
         assertTrue(removed);
-        assertEquals(favs.getFavourites().size(),4);
+        assertEquals(Favourites.getInstance().getmFavourites().size(),4);
     }
 
     @Test
     public void isFavourite_returns_correct_boolean() throws Exception {
-        Favourites favs = new Favourites();
         List<Recipe> mr = MockRecipeData.getRecipes();
-        favs.setFavourites(mr);
-        assertTrue(favs.isFavourite(mr.get(0)));
+        Favourites.getInstance().setmFavourites(mr);
+        assertTrue(Favourites.getInstance().isFavourite(mr.get(0)));
     }
-    // TODO add tests for toJSON / fromJSON methods.
 }
+// TODO add tests for toJSON / fromJSON methods.
+
+//    @Test
+//    public void toJson_returns_jsonstring() throws Exception {
+//        Favourites favs = new Favourites();
+//        favs.setFavourites(MockRecipeData.getRecipes());
+//        String json = favs.toJson();
+//        Favourites nfavs = new Favourites();
+//        nfavs.fromJson(json);
+//        assertEquals(nfavs.getFavourites().contains(MockRecipeData.getRecipes().get(0)), true);
+//    }
+//}
