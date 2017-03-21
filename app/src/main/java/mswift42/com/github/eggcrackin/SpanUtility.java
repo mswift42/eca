02cd1e0b053b45dd3a5c9e54d18944b25fcabd5b
mswift42 <martin.haesler@gmail.com>
2017-03-21 11:48:16 +0000

    Divide dpwidth by 300.

--- app/src/main/java/mswift42/com/github/eggcrackin/SpanUtility.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/SpanUtility.java
@@ -1,5 +1,14 @@
 package mswift42.com.github.eggcrackin;
 
 
+import android.content.Context;
+import android.util.DisplayMetrics;
+
 public class SpanUtility {
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int columns = (int) (dpWidth / 300);
+        return columns;
+    }
 }
