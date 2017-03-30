cdc501da39d4294d2a2646edbd24bea127370329
mswift42 <martin.haesler@gmail.com>
2017-03-30 17:31:34 +0100

    Implement Parcelable

--- app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/Favourites.java
@@ -1,5 +1,8 @@
 package mswift42.com.github.eggcrackin;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -7,9 +10,9 @@ import java.util.Collection;
 import java.util.HashSet;
 
 
-public class Favourites {
+public class Favourites implements Parcelable {
     private static final Favourites ourInstance = new Favourites();
-    private ArrayList<String> favourites = new ArrayList<>();
+    private ArrayList<Recipe> favourites = new ArrayList<>();
 
     public static Favourites getInstance() {
         return ourInstance;
@@ -53,4 +56,13 @@ public class Favourites {
         favourites = favs.getFavourites();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
 }
