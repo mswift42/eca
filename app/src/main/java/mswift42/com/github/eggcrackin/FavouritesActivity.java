204fcf03e63c1225f1d90a01e79f36926e0fe564
mswift42 <martin.haesler@gmail.com>
2017-04-04 17:36:12 +0100

    Set layoutManager for recyclerview.

--- app/src/main/java/mswift42/com/github/eggcrackin/FavouritesActivity.java
+++ app/src/main/java/mswift42/com/github/eggcrackin/FavouritesActivity.java
@@ -2,12 +2,31 @@ package mswift42.com.github.eggcrackin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class FavouritesActivity extends AppCompatActivity {
+public class FavouritesActivity extends AppCompatActivity implements FavouritesAdapter.ListItemClickListener {
+
+    private FavouritesAdapter mAdapter;
+    private RecyclerView mFavourites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+
+        mFavourites = (RecyclerView) findViewById(R.id.ec_favourites_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mFavourites.setLayoutManager(layoutManager);
+
+        mAdapter = new FavouritesAdapter(Favourites.getInstance().getFavourites().size(), this);
+        mFavourites.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
     }
 }
